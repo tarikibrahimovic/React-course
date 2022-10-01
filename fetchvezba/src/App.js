5794407130942930bcd3004fd3841a700288e0d5
Tarik <tarikibrahimovic2016@gmail.com>
2022-10-01 16:33:02 +0200

    testing apis

--- fetchvezba/src/App.js
+++ fetchvezba/src/App.js
@@ -8,30 +8,47 @@ function App() {
   let value = "";
 
   const getUser = (el) => {
-    const options = {
-      // method: "GET",
-      // url: `https://numbersapi.p.rapidapi.com/${el}/date`,
-      // params: { fragment: "true", json: "true" },
-      // headers: {
-      //   "X-RapidAPI-Key": "07aedaa6bdmsh5086c5b7fe24ecep1ca830jsnc57d502e51b1",
-      //   "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
-      // },
-      method: 'GET',
-  url: 'https://free-football-soccer-videos.p.rapidapi.com/',
-  headers: {
-    'X-RapidAPI-Key': '07aedaa6bdmsh5086c5b7fe24ecep1ca830jsnc57d502e51b1',
-    'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com'
-  }
-    };
-    axios
-      .request(options)
-      .then(function (response) {
-        setData(response.data);
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+  //   const options = {
+  //     // method: "GET",
+  //     // url: `https://numbersapi.p.rapidapi.com/${el}/date`,
+  //     // params: { fragment: "true", json: "true" },
+  //     // headers: {
+  //     //   "X-RapidAPI-Key": "07aedaa6bdmsh5086c5b7fe24ecep1ca830jsnc57d502e51b1",
+  //     //   "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
+  //     // },
+  //     method: 'GET',
+  // url: 'https://free-football-soccer-videos.p.rapidapi.com/',
+  // headers: {
+  //   'X-RapidAPI-Key': '07aedaa6bdmsh5086c5b7fe24ecep1ca830jsnc57d502e51b1',
+  //   'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com'
+  // }
+  //   };
+
+
+  // const options = "https://apiv2.allsportsapi.com/football/?get=Countries&APIkey=fb1a25cf8f16d4c6ab5adf05526cf395e1e791847bafb4aecb6cf6fe076474e1";
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       setData(response.data);
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+
+
+  //fb1a25cf8f16d4c6ab5adf05526cf395e1e791847bafb4aecb6cf6fe076474e1
+    // fetch("https://kitsu.io/api/edge/anime?filter[text]=attack")
+    fetch("https://kitsu.io/api/edge/anime-characters/4")
+    .then((res) => {
+      return res.json();
+    }).then((response) => {
+      console.log(response);
+      setData(response);
+    }).catch((e) => {
+      console.log(e);
+    })
+
   };
 
   // useEffect(() => {
@@ -55,7 +72,7 @@ function App() {
           Submit
         </button>
       </div>
-      <h1>{data.text}</h1>
+      {/* <h1>{data.text}</h1> */}
     </div>
   );
 }
